fe50fa73b743d91b2bad008701d9e47355a5bd0a
SyedaGhousia <35478870+SyedaGhousia@users.noreply.github.com>
2018-04-22 13:24:43 +0400

    Syeda: committing home chnage in header

--- includes/header.php
+++ includes/header.php
@@ -18,7 +18,7 @@
                <span class="icon-bar"></span>
                <span class="icon-bar"></span>
                </button>
-               <a class="navbar-brand" href="page_view/home">Naresco Travels</a>
+               <a class="navbar-brand" href="home">Naresco Travels</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
