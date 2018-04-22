343a5dbb4128a4e8e04dd1d67db2e68ca97ffa03
SyedaGhousia <35478870+SyedaGhousia@users.noreply.github.com>
2018-04-22 13:21:16 +0400

    Syeda: committing htaccess changes

--- includes/header.php
+++ includes/header.php
@@ -23,9 +23,9 @@
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                <ul class="nav navbar-nav navbar-right">              
-                  <li><a href="page_view/about-us">About Us</a></li>
-                  <li><a href="page_view/services">Services</a></li>
-                  <li><a href="page_view/contact">Contact</a></li>
+                  <li><a href="about-us">About Us</a></li>
+                  <li><a href="services">Services</a></li>
+                  <li><a href="contact">Contact</a></li>
                </ul>
             </div>
             <!-- /.navbar-collapse -->
